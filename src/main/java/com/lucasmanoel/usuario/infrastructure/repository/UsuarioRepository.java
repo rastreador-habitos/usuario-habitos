6ef3bdcb305a0fc2d8d94d52f8b3fc721d468dd9
Lucas <lucasmanoel.email@gmail.com>
2026-05-07 22:52:35 -0300

    Criação de todos os endpoints, security, banco de dados, exceptions, logica

---
+++ src/main/java/com/lucasmanoel/usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.lucasmanoel.usuario.infrastructure.repository;
+
+import com.lucasmanoel.usuario.infrastructure.entity.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+    boolean existesByEmail(String email);
+
+    Optional<UsuarioEntity> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
---
+++ src/main/java/com/lucasmanoel/usuario/infrastructure/security/JwtUtil.java
@@ -0,0 +1,61 @@
+package com.lucasmanoel.usuario.infrastructure.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+
+@Service
+public class JwtUtil {
+
+    // Chave secreta usada para assinar e verificar tokens JWT
+    private static final String secretKey = "8d82e629-1141-47cb-9a0e-ff303f7a5a83";
+
+    private SecretKey getSecretKey(){
+        byte[] key = Base64.getDecoder().decode(secretKey);
+        return Keys.hmacShaKeyFor(key);
+    }
+
+    // Gera um token JWT com o nome de usuário e validade de 1 hora
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username) // Define o email de usuário como o assunto do token
+                .issuedAt(new Date()) // Define a data e hora de emissão do token
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // Define a data e hora de expiração (1 hora a partir da emissão)
+                .signWith(getSecretKey())// Converte a chave secreta em bytes e assina o token com ela
+                .compact(); // Constrói o token JWT
+    }
+
+    // Extrai as claims do token JWT (informações adicionais do token)
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey()) // Define a chave secreta para validar a assinatura do token
+                .build()
+                .parseSignedClaims(token) // Analisa o token JWT e obtém as claims
+                .getPayload(); // Retorna o corpo das claims
+    }
+
+    // Extrai o nome de usuário do token JWT
+    public String extrairEmailToken(String token) {
+        // Obtém o assunto (nome de usuário) das claims do token
+        return extractClaims(token).getSubject();
+    }
+
+    // Verifica se o token JWT está expirado
+    public boolean isTokenExpired(String token) {
+        // Compara a data de expiração do token com a data atual
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    // Valida o token JWT verificando o nome de usuário e se o token não está expirado
+    public boolean validateToken(String token, String username) {
+        // Extrai o nome de usuário do token
+        final String extractedUsername = extrairEmailToken(token);
+        // Verifica se o nome de usuário do token corresponde ao fornecido e se o token não está expirado
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+}
